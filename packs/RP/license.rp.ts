21540884e3478ececcf559776b4f6b3d95bdd607
theaddon <theaddonn@gmail.com>
2026-05-10 04:30:42 +0200

    fix: update license copying dirs

--- packs/RP/license.rp.ts
+++ packs/RP/license.rp.ts
@@ -1,1 +1,2 @@
-Deno.copyFileSync('../../../LICENSE.md', 'LICENSE.md');
+Deno.copyFileSync('../../../LICENSES/CC-BY-NC-SA-4.0.txt', 'CC-BY-NC-SA-4.0.txt');
+Deno.copyFileSync('../../../LICENSES/GPL-3.0-or-later.txt', 'GPL-3.0-or-later.txt');
