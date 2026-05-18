063479eaeef67ad93be2d1eaec0ed559a9bb1ec9
Lucy <theaddonn@gmail.com>
2026-05-18 09:26:48 +0200

    Refactor license file paths and add trademark copy
    Updated file path handling for license files and added trademark file copy.

--- packs/RP/license.rp.ts
+++ packs/RP/license.rp.ts
@@ -1,6 +1,7 @@
 import { join } from 'jsr:@std/path@^1';
 
-const dir = join(Deno.env.get('ROOT_DIR')!, 'LICENSES');
+const dir = Deno.env.get('ROOT_DIR')!;
 
-Deno.copyFileSync(join(dir, 'CC-BY-NC-SA-4.0.txt'), 'CC-BY-NC-SA-4.0.txt');
-Deno.copyFileSync(join(dir, 'GPL-3.0-or-later.txt'), 'GPL-3.0-or-later.txt');
+Deno.copyFileSync(join(dir, 'LICENSES', 'CC-BY-NC-SA-4.0.txt'), 'CC-BY-NC-SA-4.0.txt');
+Deno.copyFileSync(join(dir, 'LICENSES', 'GPL-3.0-or-later.txt'), 'GPL-3.0-or-later.txt');
+Deno.copyFileSync(join(dir, 'TRADEMARK.md'), 'TRADEMARK.md');
